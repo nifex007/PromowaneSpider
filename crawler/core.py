323635c6c9da7346c3bf721b6bea33ca3ca7d231
Nifemi Sola-Ojo <nifex007@yahoo.com>
2022-07-25 22:49:54 +0100

    place holder commit

--- crawler/core.py
+++ crawler/core.py
@@ -1,20 +1,23 @@
 
-from cgi import print_exception
+from requests import Response
 import scrapy
 from scrapy.http import Request
-from helpers import format_price, write_to_file, encode_text
-import json
+import requests
+from helpers import format_price, write_to_file, get_description
+from bs4 import BeautifulSoup
+
    
 class OlxPromowaneSpider(scrapy.Spider):
     name = 'Ogłoszenia promowane'
     start_urls = [
         'https://olx.pl',
     ]
+    headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
 
     def start_requests(self):
-        headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+        
         for url in self.start_urls:
-            yield Request(url, headers=headers)
+            yield Request(url, headers=self.headers)
 
     def parse(self, response):
         mainpageAds = "//*[@id='mainpageAds']/ul/li"
@@ -25,25 +28,72 @@ class OlxPromowaneSpider(scrapy.Spider):
             a_href_attribute = ad.xpath(".//@href")
             img_src_attribute = a_tag.xpath("./img/@src")
             title = a_title_attribute.get()
+            
             description = a_href_attribute.get()
+            
             price = ad.css(".price::text").get()
             image = img_src_attribute.get()
+            link = ad.css("a::attr(href)").get()
+
+            print("link", link)
             
-            print('title', title)
-            print("description", description)
-            print("image", image)
-            print("original price", price)
-            print("formated_price", format_price(price))
 
             yield dict_list.append(
                 {
-                    "title" : encode_text(title),
+                    "title" : title,
                     "description" : description,
                     "image": image,
                     "price": format_price(price)
                 }
             )
+            
+            
+            yield response.follow(description, callback=self.parse_description, headers=self.headers)
 
-        write_to_file("output.json", dict_list)
+
+
+            
+
+            # get_description(description, self.headers)
+
+
+        write_to_file("output.json", dict_list, "json")
+    
+    def parse_description(self, response):
+        # print()
+        # css-g5mtbi-Text
+        # print("response", response)
+        req = requests.get(response.url)
+        
+        # print(html)
+
+        soup = BeautifulSoup(req.text, 'html.parser')
+
+        write_to_file("test.html", soup.prettify(), "html")
+        # description = soup.find_all("div", {"class": "css-g5mtbi-Text"})
+        # print("soup", description)
+        # print("pretty", soup.prettify())
+        # body = soup.body
+        # description = soup.find_all(attrs={"data-cy": "ad_description"})
+        # print("div", body("p", {"class":"css-g5mtbi-Text"}))
+        # print("contents", body.contents)
+        # print("soup", description)
+        # print("contents", body.c)
+        
+
+        pass
+
         
         
+# def parse(self, response):
+#         for href in response.css('div.post-header h2 a::attr(href)').getall():
+#             yield scrapy.Request(href)
+#         yield scrapy.Request(
+#             url=response.css('a.next-posts-link::attr(href)').get(),
+#             callback=self.parse_blog_post,
+#         )
+#     def parse_blog_post(self, response):
+#         yield {
+#             'url': response.url,
+#             'title': response.css('span#hs_cos_wrapper_name::text').get(),
+#         }
--- crawler/helpers.py
+++ crawler/helpers.py
@@ -1,6 +1,9 @@
 import re
 from decimal import Decimal
 import json
+from scrapy.http import Request, Response
+from bs4 import BeautifulSoup
+
 
 
 def format_price(raw_price: str) -> float:
@@ -12,11 +15,29 @@ def format_price(raw_price: str) -> float:
     
     return round(float(Decimal(price_string)), 2)
 
-def write_to_file(file_name:str, dict_list:list):
-    json_data = json.dumps(dict_list, indent=4)
+def write_to_file(file_name:str, doc, format="json"):
+    if format=="json":
+        doc = json.dumps(doc, indent=4)
     
     with open(file_name, "w", encoding="utf-8") as outfile:
-        outfile.write(json_data)
+        outfile.write(doc)
+
+def get_description(url, headers):
+    # response = Request(url, headers=headers)
+    # description = response.xpath("//*[@id='root']/div[1]/div[3]/div[3]/div[1]/div[2]/div[7]/div").get()
+    
+    # response = Response(url=url)
+    # print(response.text)
+
+    # description = response.xpath("//*[@id='root']/div[1]/div[3]/div[3]/div[1]/div[2]/div[7]/div").get()
+    # print(description)
+
+    # soup = BeautifulSoup(url, 'html.parser')
+    # print("soup", soup)
+    # pass
+    pass
+
+
 
     
 
